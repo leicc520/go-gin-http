92e0d96fa36a13b1b4bf87092f825c84dac09688
leicc520 <leicc123@gmail.com>
2022-07-27 18:44:41 +0800

    1111

--- tracer.go
+++ tracer.go
@@ -2,6 +2,8 @@ package core
 
 import (
 	"context"
+	"io"
+
 	"github.com/gin-gonic/gin"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
@@ -9,37 +11,61 @@ import (
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 )
 
-func init() {
-	jcfg := jaegercfg.Configuration{
-		Sampler: &jaegercfg.SamplerConfig{
-			Type:  "const",
-			Param: 1,
-		},
-		ServiceName: "serviceName",
-	}
+const JaegerTracerCTX = "TracerCTX"
 
-	report := jaegercfg.ReporterConfig{
-		LogSpans:           true,
-		LocalAgentHostPort: "locahost:6831",
-	}
+var JaegerTracer *JaegerTracingSt = nil
 
-	reporter, _ := report.NewReporter(serviceName, jaeger.NewNullMetrics(), jaeger.NullLogger)
-	tracer, closer, _ := jcfg.NewTracer(
-		jaegercfg.Reporter(reporter),
-	)
-	//ctx = gtx.Get("ctx").(context.Context)
+type JaegerTracingSt struct {
+	jConfig   jaegercfg.Configuration
+	jReporterConfig jaegercfg.ReporterConfig
+	jReporter jaeger.Reporter
+	JTracer  opentracing.Tracer
+	JCloser  io.Closer
+}
+
+//初始化全局的tracing对象处理逻辑
+func IniJaegerTracing(serviceName, agentHost string) *JaegerTracingSt {
+	if JaegerTracer == nil {//初始化的情况
+		JaegerTracer = &JaegerTracingSt{jConfig: jaegercfg.Configuration{
+			Sampler: &jaegercfg.SamplerConfig{
+				Type:  "const",
+				Param: 1,
+			},
+			ServiceName: serviceName,
+		}, jReporterConfig: jaegercfg.ReporterConfig{
+			LogSpans:           true,
+			LocalAgentHostPort: agentHost,
+		}}
+		JaegerTracer.jReporter, _ = JaegerTracer.jReporterConfig.NewReporter(serviceName, jaeger.NewNullMetrics(), jaeger.NullLogger)
+		JaegerTracer.JTracer, JaegerTracer.JCloser, _ = JaegerTracer.jConfig.NewTracer(jaegercfg.Reporter(JaegerTracer.jReporter))
+	}
+	return JaegerTracer
+}
+
+//获取跟踪数据上下文信息
+func GetTracingCtx(c *gin.Context) context.Context {
+	if oCtx, isExist := c.Get("ctx"); isExist {
+		if ctx, ok := oCtx.(context.Context); ok {
+			return ctx
+		}
+	}
+	return nil
 }
 
 func GINTracing() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		path := c.Request.URL.Path
-		span := tracer.StartSpan(path, ext.SpanKindRPCServer)
-		ext.HTTPUrl.Set(span, path)
-		ext.HTTPMethod.Set(span, ctx.Request.Method)
-		traceCtx := opentracing.ContextWithSpan(context.Background(), span)
-		c.Set("ctx", traceCtx)
-		c.Next()
-		ext.HTTPStatusCode.Set(span, uint16(c.Writer.Status()))
-		span.Finish()
+		if JaegerTracer != nil {
+			path := c.Request.URL.Path
+			span := JaegerTracer.JTracer.StartSpan(path, ext.SpanKindRPCServer)
+			ext.HTTPUrl.Set(span, path)
+			ext.HTTPMethod.Set(span, c.Request.Method)
+			traceCtx := opentracing.ContextWithSpan(context.Background(), span)
+			c.Set(JaegerTracerCTX, traceCtx)
+			c.Next()
+			ext.HTTPStatusCode.Set(span, uint16(c.Writer.Status()))
+			span.Finish()
+		} else {
+			c.Next()
+		}
 	}
 }
