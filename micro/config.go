8fb341be687d9de7ea54a2c7656056bc532c21f7
chenchun_lei <chenchun_lei@fzzixun.com>
2022-09-07 10:14:00 +0800

    加载配置数据信息

--- micro/config.go
+++ micro/config.go
@@ -38,47 +38,53 @@ func (c *Config) Load(confName string, config interface{}) *Config {
 }
 
 //加载配置 数据资料信息
-func (c *Config)LoadFile(confFile string, config interface{}) *Config {
+func LoadFile(confFile string, config interface{}) ([]byte, error) {
 	if confFile == "" {
 		confFile = "config/default.yml"
 	}
 	if file, err:=os.Stat(confFile); err != nil || !file.Mode().IsRegular() {
-		log.Write(log.ERROR, "load Config File Failed: ", err)
+		return nil, err
 	}
 	data, _ := ioutil.ReadFile(confFile)
 	//把yaml形式的字符串解析成struct类型 先子类初始化
 	if err := yaml.Unmarshal(data, config); err != nil {
-		log.Write(log.ERROR, "load Config child Parse Failed: ", err)
+		return nil, err
 	}
-	//把yaml形式的字符串解析成struct类型 父类加载初始化
-	if err := yaml.Unmarshal(data, c); err != nil {
-		log.Write(log.ERROR, "load Config parent Parse Failed: ", err)
+	return 	data, nil
+}
+
+//加载配置 数据资料信息
+func (c *Config)LoadFile(confFile string, config interface{}) *Config {
+	if data, err := LoadFile(confFile, config); err != nil {
+		log.Write(log.ERROR, "load Config File Failed: ", err)
+	} else {//把yaml形式的字符串解析成struct类型 父类加载初始化
+		if err = yaml.Unmarshal(data, c); err != nil {
+			log.Write(log.ERROR, "load Config parent Parse Failed: ", err)
+		}
 	}
 	return 	c
 }
 
 //加载配置 通过配置加载数据
 func (c *Config)LoadAddr(srvAddr string, config interface{}) *Config {
-	data := NewRegSrvClient(srvAddr).Config(c.App.Name)
-	//把yaml形式的字符串解析成struct类型
-	if err := yaml.Unmarshal([]byte(data), config); err != nil {
+	if data, err := LoadAddr(srvAddr, c.App.Name, config); err != nil {
 		log.Write(log.ERROR, "load Config child Parse Failed: ", err)
-	}
-	//把yaml形式的字符串解析成struct类型
-	if err := yaml.Unmarshal([]byte(data), c); err != nil {
-		log.Write(log.ERROR, "load Config parent Parse Failed", err)
+	} else {//把yaml形式的字符串解析成struct类型
+		if err = yaml.Unmarshal([]byte(data), c); err != nil {
+			log.Write(log.ERROR, "load Config parent Parse Failed", err)
+		}
 	}
 	return 	c
 }
 
 //通过配置名称加载配置，然后解析到config配置当中
-func (c *Config)LoadConfig(srvAddr, confName string, config interface{}) error {
-	data :=NewRegSrvClient(srvAddr).Config(confName)
+func LoadAddr(srvAddr, appName string, config interface{}) (string, error) {
+	data := NewRegSrvClient(srvAddr).Config(appName)
 	//把yaml形式的字符串解析成struct类型
 	if err := yaml.Unmarshal([]byte(data), config); err != nil {
-		return err
+		return "", err
 	}
-	return nil
+	return data, nil
 }
 
 //通过远程配置服务器加载
