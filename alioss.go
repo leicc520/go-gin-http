82734edc3f45a08bb7c7c96a44c3578e3f575403
chenchun_lei <chenchun_lei@fzzixun.com>
2022-10-14 16:40:03 +0800

    新增oss组件的支持

--- alioss.go
+++ alioss.go
@@ -13,13 +13,13 @@ import (
 )
 
 type AliOssSt struct {
-	AccessKeyId     string `yaml:"accesskeyid"`
-	AccessKeySecret string `yaml:"accesskeysecret"`
+	AccessKeyId     string `yaml:"access_key_id"`
+	AccessKeySecret string `yaml:"access_key_secret"`
 	Endpoint        string `yaml:"endpoint"`
-	OssHost         string `yaml:"osshost"`
+	OssHost         string `yaml:"oss_host"`
 	Bucket          string `yaml:"bucket"`
 	BaseUrl         string `yaml:"baseurl"`
-	IsPush          string `yaml:"ispush"`
+	IsPush          string `yaml:"is_push"`
 }
 
 // 获取上传服务签名数据信息
@@ -40,19 +40,51 @@ func (s AliOssSt) UploadSign(dir string, sizeMB, expire int64) interface{} {
 	return dataSet
 }
 
+// 获取绝对地址处理逻辑
+func (s AliOssSt) Link(ossPath string) string {
+	if strings.HasPrefix(ossPath, "http") {
+		return ossPath
+	}
+	if strings.HasPrefix(s.BaseUrl, "http") {
+		return s.BaseUrl + ossPath
+	}
+	if strings.HasPrefix(s.OssHost, "http") {
+		return s.BaseUrl + ossPath
+	}
+	return s.Endpoint + ossPath
+}
+
+// 获取ACL的请求地址 私密bucket获取地址
+func (s AliOssSt) GetFile(ossPath string) (string, error) {
+	ossPath = strings.TrimLeft(ossPath, "/")
+	client, err := oss.New(s.Endpoint, s.AccessKeyId, s.AccessKeySecret)
+	if err != nil {
+		log.Write(log.ERROR, "create alioss error", err)
+		return "", err
+	}
+	// 获取存储空间。
+	bucket, err := client.Bucket(s.Bucket)
+	if err != nil {
+		log.Write(log.ERROR, "get bucket error", err)
+		return "", err
+	}
+	//上传本地文件。
+	ossPath = strings.TrimLeft(ossPath, "/")
+	return bucket.SignURL(ossPath, oss.HTTPGet, 86400, nil)
+}
+
 // oss上传文件的处理逻辑
-func AliOssUpfile(file, ossPath string, config *AliOssSt) error {
-	if config.IsPush != "on" {
+func (s AliOssSt) AliOssUpfile(file, ossPath string) error {
+	if s.IsPush != "on" {
 		return nil //系统不关闭的情况
 	}
-	client, err := oss.New(config.Endpoint,
-		config.AccessKeyId, config.AccessKeySecret)
+	client, err := oss.New(s.Endpoint, s.AccessKeyId, s.AccessKeySecret)
 	if err != nil {
 		log.Write(log.ERROR, "create alioss error", err)
 		return err
 	}
 	// 获取存储空间。
-	bucket, err := client.Bucket(config.Bucket)
+	bucket, err := client.Bucket(s.Bucket)
 	if err != nil {
 		log.Write(log.ERROR, "get bucket error", err)
 		return err
