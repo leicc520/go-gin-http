2aa8f7fe1f247e1d120f795629b3f55cecde0229
leicc520 <leicc123@gmail.com>
2022-10-29 15:38:38 +0800

    对了重新封装测试

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -6,12 +6,21 @@ import (
 	"time"
 )
 
-func TestPushRabbitMQ(t *testing.T) {
+func TestPushv1RabbitMQ(t *testing.T) {
 	q := IFQueue(&RabbitMqSt{Url: "amqp://guest:guest@192.168.138.128:5673/", AutoAck: false})
-
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 200; i++ {
 		str := fmt.Sprintf("test%08d", i)
 		err := q.Publish("test", str)
+		fmt.Println(err)
+		time.Sleep(time.Second)
+	}
+}
+
+func TestPushv2RabbitMQ(t *testing.T) {
+	q := IFQueue(&RabbitMqSt{Url: "amqp://guest:guest@192.168.138.128:5673/", AutoAck: false})
+	for i := 0; i < 200; i++ {
+		str := fmt.Sprintf("demo%08d", i)
+		err := q.Publish("demo", str)
 		fmt.Println(err)
 		time.Sleep(time.Second)
 	}
@@ -27,7 +36,9 @@ func (d *ConsumerDemo) Accept(topic string, message []byte) error {
 
 func TestConsumerRabbitMQ(t *testing.T) {
 	c := ConsumerDemo{}
+	d := ConsumerDemo{}
 	q := IFQueue(&RabbitMqSt{Url: "amqp://guest:guest@192.168.138.128:5673/", AutoAck: false})
 	q.Register("test", &c)
+	q.RegisterN("demo", 4, &d)
 	q.Start()
 }
